fead2232a23f4bbb2460bbd6d4e7037ddb03cc41
MaxDudnik32 <maxim.dudnik32@gmail.com>
2023-07-07 18:20:37 +0300

    Jest upgrading

--- src/square/square.test.js
+++ src/square/square.test.js
@@ -21,10 +21,20 @@ describe('square', () => {
         expect(square(2)).toBeLessThan(5);
         expect(square(2)).toBeGreaterThan(3);
     });
+    test('Коректное значения (Mock)', () => {
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2)
+        expect(spyMathPow).toBeCalledTimes(1);
+    });
+    test('Коректное значения (Mock trick)', () => {
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(1)
+        expect(spyMathPow).toBeCalledTimes(0);
+    });
 
     // After Each
     afterEach(() => {
-        // Delete from database
+        jest.clearAllMocks();
     })
     // After all
     afterAll(() => {
